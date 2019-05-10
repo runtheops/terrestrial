472fc3f423a49ea8b49938c3f0668b2054283f79
Sergey Motovilovets <motovilovets.sergey@gmail.com>
2019-05-10 16:08:39 +0300

    A step toward more consistent logging

--- api.py
+++ api.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+import logging
+
 from terrestrial import api
+
+
+log = logging.getLogger('api')
+log_format = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s'
+logging.basicConfig(level=logging.INFO,format=log_format)
 
 
 if __name__ == '__main__':
--- worker.py
+++ worker.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import logging
 
 from terrestrial import app
+
+
+log = logging.getLogger('worker')
+log_format = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s'
+logging.basicConfig(level=logging.INFO,format=log_format)
 
 
 if __name__ == '__main__':
